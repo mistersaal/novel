f60ed86cdbf60c0efdb0a40403e23ce0190eb3a6
Сааль Степан <mister.saal@yandex.ru>
2020-10-26 13:17:03 +0300

    Имя маршрута user

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/user', UserController::class);
+Route::get('/user', UserController::class)->name('user');
 
 Route::prefix('/novels/{novel}')->group(function () {
 
